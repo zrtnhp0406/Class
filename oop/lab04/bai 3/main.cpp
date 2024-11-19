d7dcd048d6901d2bc362cd6f4df7234f0e90f2e2
Huy Phuoc <156315187+zrtnhp0406@users.noreply.github.com>
2024-11-19 09:41:42 +0700

    Update main.cpp

--- oop/lab04/bai 3/main.cpp
+++ oop/lab04/bai 3/main.cpp
@@ -8,10 +8,14 @@ COW a;
 SHEEP b;
 DEEL c;
 int main(){
-    //ios::sync_with_stdio(0);cin.tie(0);
             cout<<"Cow: "<<a.breed()<<" "<<"con"<< '\n';
             cout<<"Sheep: "<<b.breed()<<" "<<"con"<< '\n';
             cout<<"Deel: "<<c.breed()<<" "<<"con"<< '\n';
+    cout<<"Tieng keu cua moi loai"<<'\n';
+    cout<<"Cow: "<<a.speak()<<'\n';
+    cout<<"SHEEP: "<<b.speak()<<'\n';
+    cout<<"Deel: "<<c.speak()<<'\n';
+    
     cout<<"nhap so luong su kien sinh va cho sua: ";
     cin>>event;
     for(int i=1; i<=event; i++){
